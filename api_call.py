bd2eabb1db590175c99f026b87c8f7f95bc12033
Jamie Saxon <saxon.jamie@gmail.com>
2013-10-15 00:57:57 +1100

    Massive rework to use Requests instead of curl

--- api_call.py
+++ api_call.py
@@ -1,29 +1,20 @@
 import argparse
 import subprocess
-import platform
+import datetime
 import time
+import requests
 
 # Config vars
-target = "http://pvjira01.pv.local:8001"
+target = "http://pvjira01.pv.local:8080"
 logfile = "api_call.log"
 username = "jsaxon"
 password = "aaaaaa"
 statuses =["Scheduled","In Progress","Code Review","Functional Review","Resolved","Closed"]
-days = 1
+days = 3
 start_day = -1
 
 command =' '.join(['curl -D- -u',username+':'+password,'-X GET -H "Content-Type: application/json"'])
 api_path = '/rest/api/2/search?jql='
-
-
-platform = platform.system()
-if platform == "Linux":
-   command = "curl"
-# elif platform == "Windows":
-#    command = "..."
-else:
-   print("Unsupported System")
-   exit
 
 # Arguments handler
 parser = argparse.ArgumentParser()
@@ -36,17 +27,17 @@ if args.target:
 if args.logfile:
     logfile = str(args.logfile)
      
-# jql='project=spui+AND+status+was+"Closed"+ON+endofDay(-1)'
-# run = ' ' .join([command,",target+api_path+jql,"])
-# print(run)
-# print("aaaa")
-
 loop_day = start_day
 while loop_day > start_day - days:
 #     print(loop_day)
+    day = datetime.date.fromordinal(datetime.date.today().toordinal()+loop_day)
+#     print(day)
     for status in statuses:
 #         print(status)
-         jql=' ' .join(['project=spui+AND+status+was+"'+status+'"+ON+endofDay('+str(loop_day)+')"'])
-         run = ' ' .join([command,'"'+target+api_path+jql])
-         print(run)
+         jql=' ' .join(['project=spui+AND+status+was+"'+status+'"+ON+endofDay('+str(loop_day)+')'])
+         run = ' ' .join([target+api_path+jql])
+#          print(run)
+         r = requests.get(run)
+         total = r.json().get('total')
+         print(day,status,total)
     loop_day -= 1
