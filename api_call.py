327ecf5bfe702d7c1ac06c34f6286a40f78104be
Jamie Saxon <saxon.jamie@gmail.com>
2013-10-15 18:45:50 +1100

    Cleanup and Comments

--- api_call.py
+++ api_call.py
@@ -6,42 +6,43 @@ import requests
 
 # Config vars
 target = "http://pvjira01.pv.local:8080"
-logfile = "api_call.log"
-# statuses = ["Scheduled","In Progress","Code Review","Functional Review","Resolved","Closed"]
-statuses = ["Scheduled",'In Progress","Code Review','Functional Review","Resolved","Closed']
-types = ["Bug","Task","Epic","Story"]
+# logfile = "api_call.log"
+statuses = ["Open", "ReOpened", "Scheduled", "In Progress", "Code Review", "Functional Review", "Resolved", "Closed"]
+# statuses = ["Scheduled",'In Progress","Code Review','Functional Review","Resolved","Closed']
+types = ["Bug", "Task", "Epic", "Story"]
 days = 180
 start_day = -1
-
-# command =' '.join(['curl -D- -u',username+':'+password,'-X GET -H "Content-Type: application/json"'])
 api_path = '/rest/api/2/search?jql='
 
 # Arguments handler
 parser = argparse.ArgumentParser()
 parser.add_argument("-t", "--target", help="Target Host")
-parser.add_argument("-l", "--logfile", help="Logfile")
+# parser.add_argument("-l", "--logfile", help="Logfile")
 args = parser.parse_args()
 
 if args.target:    
     target = str(args.target)
-if args.logfile:
-    logfile = str(args.logfile)
- 
-log = open("logfile", "w")
-    
-loop_day = start_day
+# if args.logfile:
+#     logfile = str(args.logfile)
+# print("Date",end="\t")
+# for status in statuses:
+#     print(status,end="\t")
+# print("Total")
+loop_day = start_day  # Initialise the Loop
 while loop_day > start_day - days:
 #     print(loop_day)
-    day = datetime.date.fromordinal(datetime.date.today().toordinal()+loop_day)
-#     print(day)
-    for status in statuses:
+    day = datetime.date.fromordinal(datetime.date.today().toordinal() + loop_day)  # Get the Day the request is for
+#     print(day,end="\t")
+    for status in statuses:  # Loop through Statuses provided
 #         print(status)
-        for type in types:
+        for type in types:  # Loop through the Types provided
 #             print(type)
-            jql=' ' .join(['project=spui+AND+issueType="'+type+'"+and+status+was+in+("'+status+'")+ON+endofDay('+str(loop_day)+')'])
-            run = ' ' .join([target+api_path+jql])
+            jql = ' ' .join(['project=spui+AND+issueType="' + type + '"+and+status+was+in+("' + status + '")+ON+endofDay(' + str(loop_day) + ')'])  # Combine the vars for the JQL query
+            run = ' ' .join([target + api_path + jql])  # combine the vars to get the full query
 #             print(run)
-            r = requests.get(run)
-            total = r.json().get('total')
-            print(day,"\t",status,"\t",type,"\t",total)
-    loop_day -= 1
+            r = requests.get(run)  # Use requests library to make a http request to the string defined in 'run'
+            total = r.json().get('total')  # Response is JSON and we are aftet the 'total' value
+            print(day, "\t", status, "\t", type, "\t", total)  # Print a tab seperated list of results
+#             print(total,end="\t")
+#     print()            
+    loop_day -= 1  # Increment the loop, we are going back in time so -1
