6f74e1a3e5bd60e59f59c09690bf4339167034bb
Jamie Saxon <saxon.jamie@gmail.com>
2013-10-16 13:58:55 +1100

    Cleaned up some old debug lines. Added support for Priorities

--- api_call.py
+++ api_call.py
@@ -10,9 +10,11 @@ target = "http://pvjira01.pv.local:8080"
 statuses = ["Open", "ReOpened", "Scheduled", "In Progress", "Code Review", "Functional Review", "Resolved", "Closed"]
 # statuses = ["Scheduled",'In Progress","Code Review','Functional Review","Resolved","Closed']
 types = ["Bug", "Task", "Epic", "Story"]
+priorities = ["Blocker", "Critical", "Major", "Minor", "Trivial"]
 days = 180
 start_day = -1
 api_path = '/rest/api/2/search?jql='
+project = "spui"
 
 # Arguments handler
 parser = argparse.ArgumentParser()
@@ -22,27 +24,16 @@ args = parser.parse_args()
 
 if args.target:    
     target = str(args.target)
-# if args.logfile:
-#     logfile = str(args.logfile)
-# print("Date",end="\t")
-# for status in statuses:
-#     print(status,end="\t")
-# print("Total")
 loop_day = start_day  # Initialise the Loop
 while loop_day > start_day - days:
-#     print(loop_day)
     day = datetime.date.fromordinal(datetime.date.today().toordinal() + loop_day)  # Get the Day the request is for
-#     print(day,end="\t")
     for status in statuses:  # Loop through Statuses provided
-#         print(status)
         for type in types:  # Loop through the Types provided
-#             print(type)
-            jql = ' ' .join(['project=spui+AND+issueType="' + type + '"+and+status+was+in+("' + status + '")+ON+endofDay(' + str(loop_day) + ')'])  # Combine the vars for the JQL query
-            run = ' ' .join([target + api_path + jql])  # combine the vars to get the full query
-#             print(run)
-            r = requests.get(run)  # Use requests library to make a http request to the string defined in 'run'
-            total = r.json().get('total')  # Response is JSON and we are aftet the 'total' value
-            print(day, "\t", status, "\t", type, "\t", total)  # Print a tab seperated list of results
-#             print(total,end="\t")
-#     print()            
+            for priority in priorities:
+                jql = ' ' .join(['project=' + project + '+AND+issueType="' + type + '"+and+priority+was+in("' + priority + '")+ON+endofDay(' + str(loop_day) + ')+and+status+was+in+("' + status + '")+ON+endofDay(' + str(loop_day) + ')'])  # Combine the vars for the JQL query
+                run = ' ' .join([target + api_path + jql])  # combine the vars to get the full query
+#                 print(run)
+                r = requests.get(run)  # Use requests library to make a http request to the string defined in 'run'
+                total = r.json().get('total')  # Response is JSON and we are aftet the 'total' value
+                print(day, "\t", status, "\t", type, "\t", priority, "\t", total)  # Print a tab seperated list of results
     loop_day -= 1  # Increment the loop, we are going back in time so -1
